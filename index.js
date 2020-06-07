1994a92ee2d665e494a545261d2fc49f7d78ba1b
sergeyman <sergey_man@ukr.net>
2020-06-08 00:03:07 +0300

    Adding JSON responce

--- index.js
+++ index.js
@@ -9,10 +9,16 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('/products', (req, res, next)=>{
-	res.send(products);
+	//res.send(products);
+	res.json({products});		//Responce Headers: Content-Type: application/json; charset=utf-8
 });
-app.get('/products', (req, res, next)=>{
-	res.send(1);
+app.get('/products/:id', (req, res, next)=>{
+	if(products[req.params.id]) {
+		res.send(products[req.params.id]);
+	}
+	else {
+		res.status(404).send('Product not found');
+	}
 });
 
 app.listen(5000, ()=> {
