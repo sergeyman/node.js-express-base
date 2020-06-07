4549405752437936133c0c6404a99260b2d51ddf
sergeyman <sergey_man@ukr.net>
2020-06-07 23:52:44 +0300

    Adding routes, nodemon

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express');
 
 const app = express();		//it's working
 
+const products = ['Apple', 'Pen', 'Computer', 'Cherry'];
+
 app.get('/', (req, res, next) => {
 	res.send('It\'s working');
+});
+
+app.get('/products', (req, res, next)=>{
+	res.send(products);
+});
+app.get('/products', (req, res, next)=>{
+	res.send(1);
 });
 
 app.listen(5000, ()=> {
