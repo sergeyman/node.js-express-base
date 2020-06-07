82323844a4212f9d91eea107476b413e288f097b
sergeyman <sergey_man@ukr.net>
2020-06-07 23:37:38 +0300

    Basic express server works

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ const express = require('express');
 
 const app = express();		//it's working
 
+app.get('/', (req, res, next) => {
+	res.send('It\'s working');
+});
 
+app.listen(5000, ()=> {
+	console.log('It\'s started');
+});
+
+
+//to start the server:
+//>node index.js
